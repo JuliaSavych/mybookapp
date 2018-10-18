aec063e0ccdfe2d9bf7cdb3e867df53416639c45
Julia Savych <julliasavych@gmail.com>
2018-10-18 17:05:38 +0300

    main content

---
+++ app/js/src.js
@@ -0,0 +1,31 @@
+//rating
+$('form').submit(function(e){
+  e.preventDefault()
+  var data = $(this).serialize()
+  alert(data)
+})
+function rateThis(wrapWidth,rateWidth){
+  var starWidth = wrapWidth/5;
+  console.log(wrapWidth)
+  return parseFloat(rateWidth/starWidth).toFixed(1); 
+}
+$('.range-raiting').mousemove(function(e){
+  if($(this).is('.done')){
+    return false;
+  }
+  var pos = e.clientX - $(e.target).offset().left;
+  $(this).find('div').width(pos)
+})
+$('.range-raiting').mouseleave(function(){
+  if($(this).is('.done')){
+    return false;
+  }
+  $(this).find('div').width(0)
+})
+$('.range-raiting').click(function(e){
+  $(this).addClass('done')
+  var pos = e.clientX - $(e.target).offset().left;
+  $(this).find('div').width(pos);
+  thisWidth = $(this).width();
+  $(this).find('input').val(rateThis($(this).width(),pos))
+})
